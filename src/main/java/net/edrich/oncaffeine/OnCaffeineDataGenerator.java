245badf9b2f861eea1c840c94429cfd78e4e599a
Edric H <edric1home@gmail.com>
2026-01-17 17:25:28 -0500

    Initial commit

---
+++ src/main/java/net/edrich/oncaffeine/OnCaffeineDataGenerator.java
@@ -0,0 +1,11 @@
+package net.edrich.oncaffeine;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class OnCaffeineDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
