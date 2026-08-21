edee598125438e84aa515f1e16ef381f8a67cf6a
Edric H <edric1home@gmail.com>
2026-08-21 08:54:57 -0400

    Directional blockstates for mugs v0.0.30dev

--- src/main/java/net/edrich/oncaffeine/block/BeverageBlock.java
+++ src/main/java/net/edrich/oncaffeine/block/BeverageBlock.java
@@ -4,10 +4,15 @@ import net.edrich.oncaffeine.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.WorldView;
+
+import java.util.Objects;
 
 public class BeverageBlock extends Block {
     public BeverageBlock(Settings settings) {
@@ -25,5 +30,14 @@ public class BeverageBlock extends Block {
                 && !(world.getBlockState(pos.down(1)).isIn(ModTags.Blocks.CUP_BLOCKS_UNPLACEABLE));
 
 
+    }
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(Properties.FACING);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext context) {
+        return Objects.requireNonNull(super.getPlacementState(context)).with(Properties.FACING, context.getHorizontalPlayerFacing().getOpposite());
     }
 }
--- src/main/java/net/edrich/oncaffeine/block/ClassicTeaKettleBlock.java
+++ src/main/java/net/edrich/oncaffeine/block/ClassicTeaKettleBlock.java
@@ -16,7 +16,6 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
