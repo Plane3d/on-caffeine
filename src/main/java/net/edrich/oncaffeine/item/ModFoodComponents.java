4640e601928fd3e5559b71c80e35eb5d8af9adcc
Edric H <edric1home@gmail.com>
2026-08-25 09:17:26 -0400

    Updated status effect lengths for drinks v0.0.32dev

--- src/main/java/net/edrich/oncaffeine/item/ModFoodComponents.java
+++ src/main/java/net/edrich/oncaffeine/item/ModFoodComponents.java
@@ -6,21 +6,21 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent COFFEE_MEDIUM = new FoodComponent.Builder().hunger(1).saturationModifier(4.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 200, 2), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 200, 2), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 3600, 2), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 3600, 2), 1.0f)
             .alwaysEdible().build();
     public static final FoodComponent GREEN_MEDIUM = new FoodComponent.Builder().hunger(1).saturationModifier(2.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 200, 0), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 200, 0), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 1), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 3600, 0), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 3600, 0), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 240, 1), 1.0f)
             .alwaysEdible().build();
     public static final FoodComponent BLACK_MEDIUM = new FoodComponent.Builder().hunger(1).saturationModifier(2.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 200, 1), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 200, 1), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 0), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 3600, 1), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.JUMP_BOOST, 3600, 1), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 240, 0), 1.0f)
             .alwaysEdible().build();
     public static final FoodComponent HERBAL_MEDIUM = new FoodComponent.Builder().hunger(1).saturationModifier(2.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 100, 1), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200, 1), 1.0f)
             .alwaysEdible().build();
 
 
